fbe590422e98b20218f5f8b3280f53fcfc13ed94
Pablo Alves <PabloHAlves99@gmail.com>
2024-03-11 17:30:48 -0300

    add filter

--- contact/admin.py
+++ contact/admin.py
@@ -5,17 +5,17 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
-    ordering = 'id',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
+    ordering = '-id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 100
-    # list_editable = 'phone'
+    list_editable = 'show',
     list_display_links = 'id', 'first_name',
 
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = 'name',
-    ordering = 'id',
+    ordering = '-id',
