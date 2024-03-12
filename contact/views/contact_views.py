69eafd67f23727faeff6d98115d950208a895834
Pablo Alves <PabloHAlves99@gmail.com>
2024-03-11 22:27:56 -0300

    Adding filters

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 # from django.http import Http404
 from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect("contact:index")
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value)
+            # Q(email__icontains=search_value)
+        )\
+        .order_by('first_name')
 
     context = {
         'contacts': contacts,
