5a7f29556c800035f20dc75c2755424d6df266ad
Pablo Alves <PabloHAlves99@gmail.com>
2024-03-11 21:45:10 -0300

    title

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -23,9 +24,10 @@ def contact(request, contact_id):
     # if single_contact is None:
     #     raise Http404()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
-
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(
