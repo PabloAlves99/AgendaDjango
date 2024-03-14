920aa290739545761df81b9f8d325e81116de1a2
Pablo Alves <PabloHAlves99@gmail.com>
2024-03-13 23:46:41 -0300

    Paginação

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
-# from django.http import Http404
+from django.core.paginator import Paginator
 from contact.models import Contact
+# from django.http import Http404
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -35,8 +40,12 @@ def search(request):
         )\
         .order_by('first_name')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
         'search_value': search_value,
     }
